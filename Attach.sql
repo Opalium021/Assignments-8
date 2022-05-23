170b2797989e59983bf07869942415ede5b95924
Javad Rahmaty <60297604+Opalium021@users.noreply.github.com>
2022-05-23 22:10:36 +0430

    Update Attach.sql

--- Attach.sql
+++ Attach.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE Assets
-       ON (FILENAME = 'F :\MyProjects\assets.mdf '),
-       (FILENAME = 'F :\MyProjects\Assets_log.ldf ' )
+       ON (FILENAME = 'F :\MyProjects\assets.mdf'),
+       (FILENAME = 'F :\MyProjects\Assets_log.ldf')
        FOR ATTACH ;
